c3843b63e48d938c7e469f9052c200fed27ddf59
YutamaKotaro <california.dreamin.never.die@gmail.com>
2017-01-30 01:56:11 +0900

    push currente src

--- packages/domain/index.js
+++ packages/domain/index.js
@@ -1,5 +1,6 @@
 // @flow
 import domain from 'base-domain-loopback'
+
 export function plus(a: number, b: number): number {
     return a + b
 }
--- packages/mobile/index.ios.js
+++ packages/mobile/index.ios.js
@@ -3,5 +3,4 @@
 import React from 'react'
 import { AppRegistry } from 'react-native'
 import Shared from './src/components/shared'
-
 AppRegistry.registerComponent('mobile', () => Shared)
